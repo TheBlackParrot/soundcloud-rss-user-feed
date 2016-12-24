5966cee3deaea5d11418f1ea218b4883ad211cf0
TheBlackParrot <theblackparrot0@gmail.com>
2016-12-24 01:46:33 -0600

    can now parse html on API failure

--- cacher.py
+++ cacher.py
@@ -1,7 +1,13 @@
+from __future__ import unicode_literals;
 import soundcloud;
 import json;
 import sqlite3;
 import os;
+import urllib.request;
+try: 
+	from BeautifulSoup import BeautifulSoup;
+except ImportError:
+	from bs4 import BeautifulSoup;
 
 import settings;
 
@@ -20,37 +26,96 @@ for row in sql_cur.execute("SELECT USER_ID FROM following"):
 
 	num = 0;
 	tracks = sc_client.get(api_query, limit=15);
-	for track in tracks:
-		user = track.user;
+	if not len(tracks):
+		'''
+			currently waiting on https://github.com/rg3/youtube-dl/issues/11181
 
-		file_location = "{}/{}/{}.json".format(settings.cache_directory, user['id'], track.id);
-		dir_location = "{}/{}".format(settings.cache_directory, user['id']);
+			UPDATE: i give up, i'm scraping artist pages now
+		'''
 
-		if num == 0:
-			print("checking {}...".format(user['permalink']));
+		print("no API response for {}, probably blocked.".format(row['USER_ID']));
 
-		num += 1;
+		user = sc_client.get('/users/{}'.format(row['USER_ID']));
+		tracks_page = '{}/tracks'.format(user.permalink_url);
 
-		if os.path.exists(file_location):
-			continue;
-
-		info = {
-			"id": track.id,
-			"title": track.title,
-			"duration": track.duration,
-			"timestamp": track.created_at,
-			"description": track.description,
-			"permalink": track.permalink_url,
-			"user": {
-				"id": user['id'],
-				"name": user['username'],
-				"permalink": user['permalink_url'],
-				"username": user['permalink']
-			}
-		};
+		dir_location = "{}/{}".format(settings.cache_directory, user.id);
 
 		if not os.path.exists(dir_location):
 			os.makedirs(dir_location);
 
-		with open(file_location, "w") as file:
-			json.dump(info, file, **settings.json_settings);
+		if num == 0:
+			print("scraping {}...".format(user.permalink));
+
+		num += 1;
+
+		with urllib.request.urlopen(tracks_page) as response:
+			html = response.read();
+
+			soup = BeautifulSoup(html, "html.parser");
+
+			for article in soup.find_all(class_='audible'):
+				track = article.find('a', attrs={"itemprop": "url"}, href=True);
+				timestamp = article.find('time');
+
+				file_location = "{}/{}.json".format(dir_location, track['href'].split("/")[-1:][0]);
+
+				if os.path.exists(file_location):
+					continue;
+
+				info = {
+					"id": -1,
+					"title": track.text,
+					"duration": -1,
+					"timestamp": timestamp.text,
+					"description": "API request was rejected, unable to scrape for description.",
+					"permalink": "http://soundcloud.com{}".format(track['href']),
+					"user": {
+						"id": -1,
+						"name": user.username,
+						"permalink": user.permalink_url,
+						"username": user.permalink
+					}
+				};
+
+				with open(file_location, "w") as file:
+					json.dump(info, file, **settings.json_settings);
+
+			print("finished");
+
+	else:
+		for track in tracks:
+			user = track.user;
+
+			file_location = "{}/{}/{}.json".format(settings.cache_directory, user['id'], track.id);
+			dir_location = "{}/{}".format(settings.cache_directory, user['id']);
+
+			if num == 0:
+				print("checking {}...".format(user['permalink']));
+
+			num += 1;
+
+			if os.path.exists(file_location):
+				continue;
+
+			info = {
+				"id": track.id,
+				"title": track.title,
+				"duration": track.duration,
+				"timestamp": track.created_at,
+				"description": track.description,
+				"permalink": track.permalink_url,
+				"user": {
+					"id": user['id'],
+					"name": user['username'],
+					"permalink": user['permalink_url'],
+					"username": user['permalink']
+				}
+			};
+
+			if not os.path.exists(dir_location):
+				os.makedirs(dir_location);
+
+			with open(file_location, "w") as file:
+				json.dump(info, file, **settings.json_settings);
+
+		print("finished");
--- web/list.php
+++ web/list.php
@@ -4,6 +4,8 @@
 
 	$iterator = new DirectoryIterator($cache_dir);
 
+	$leading = "http://192.168.1.150/soundcloud2rss/web/";
+
 	foreach($iterator as $file) {
 		if($file->isDot()) {
 			continue;
@@ -11,7 +13,11 @@
 
 		if($file->isDir()) {
 			$id = $file->getBasename();
-			echo "<a href=rss.php?user_id=$id>rss.php?user_id=$id</a><br/>";
+			if($_GET['static']) {
+				echo "<a href={$leading}rss.php?user_id=$id>{$leading}rss.php?user_id=$id</a><br/>";
+			} else {
+				echo "<a href=rss.php?user_id=$id>rss.php?user_id=$id</a><br/>";
+			}
 		}
 	}
 ?>
